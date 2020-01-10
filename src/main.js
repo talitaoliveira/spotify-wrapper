6610d3f485394d63ebe52cc67cc4d699205de8b8
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-10 15:31:03 -0300

    Add husky

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const a = 3;
+
+const b = 2;
+
+const e = 4;
+
+const c = 'dsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdidsaiudhuhidshiuahsdi';
+
+
+function teste() {
+  return a + b + e + c;
+}
+
+teste();
