58764659c0e2e57b610a827c20e0a0d099a08430
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-15 16:26:55 -0300

    Implementing FizzBuz

--- tests/fizzbuzz.spec.js
+++ tests/fizzbuzz.spec.js
@@ -12,8 +12,17 @@ Se não for múltiplo de nada, retorna o número
 
  describe('Main', () => {
 
-  it('Should return `fizz`when multiple of 3', () =>{
+  it('Should return `Fizz` when multiple of 3', () =>{
     expect(FizzBuzz(3)).to.be.equal('Fizz')
+    expect(FizzBuzz(6)).to.be.equal('Fizz')
+  })
+
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz')
+  })
+
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz')
   })
 
  })
